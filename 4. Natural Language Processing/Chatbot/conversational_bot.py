d11b8aa569a2a38dcbca71c756ef3db5eade366c
Nikhil Reddy <nikhilbadveli6@gmail.com>
2022-04-06 21:32:23 +0100

    commit before deletion

---
+++ 4. Natural Language Processing/Chatbot/conversational_bot.py
@@ -0,0 +1,6 @@
+"""
+We will use DialoGPT for this task.
+DialoGPT is a large-scale tunable neural conversational response generation model trained on 147M conversations
+extracted from Reddit.
+The good thing is that you can fine-tune it with your dataset to achieve better performance than training from scratch.
+"""
